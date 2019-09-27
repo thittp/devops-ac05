185596843c4e572d606bc1e923b242ddec89dae8
Erick Soares dos Santos <1900538@SALAS.AULAS>
2019-09-26 21:33:30 -0300

    Subindo

--- com/ac5/test/test_converte_hora.py
+++ com/ac5/test/test_converte_hora.py
@@ -2,5 +2,8 @@ import pytest
 from com.ac5.cvHora import converteHora
 
 
-def convertHora():
-    assert converteHora(20, 50) == '20:50 PM', ("O resultado tem que ser 20:50 PM")
+def test_convertHora():
+    assert converteHora(20, 50) == '08:50 PM', ("O resultado tem que ser 08:50 PM")
+    assert converteHora(60, 70) == None, ("O resultado tem que ser None")
+    assert converteHora(11, 30) == '11:30 AM', ("O resultado tem que ser 11:30 AM")
+    assert converteHora(0, 0) == '12:00 AM', ("O resultado tem que ser 12:00 AM")
